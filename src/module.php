74818eb7188d595a1fa3a31528dd9270830a225a
[Arif <arif.hussoun898@gmail.com>
2026-02-17 11:10:31 +0500

    feature: sum and sub function added

---
+++ src/module.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "the new module to start";
+
+function module(){
+    echo "module function is in working stage";
+}
+function module2(){
+    echo "module function is in working stage";
+}
