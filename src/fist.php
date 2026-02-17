13213bfa250019b7ab0c99c47ed7a205cc50e036
[Arif <arif.hussoun898@gmail.com>
2026-02-17 14:15:23 +0500

    feat: multiplication function is added

--- src/fist.php
+++ src/fist.php
@@ -6,7 +6,6 @@
 
 
  function greeting($name){
-<<<<<<< HEAD
     echo "hello Mr. ".$name."! how are you \n";
  }
 
@@ -16,8 +15,6 @@
     echo $message."!".$name;
  }
 
-
-
  //function calls 
  greeting($name);
  greetCostumMessage("Good morning", "Kasim \n");
@@ -25,9 +22,3 @@
 
 
  
-=======
-    echo "hello Mr. ".$name."howt are you ";
- }
-
- greeting($name);
->>>>>>> a33264c96aa5572a53d520e2adcb283693d4dfcd
