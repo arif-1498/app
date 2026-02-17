8d5109abee82e36b91874b9153a84964fd4e0f22
[Arif <arif.hussoun898@gmail.com>
2026-02-17 11:34:27 +0500

    division function added

---
+++ src/NewChang.php
@@ -0,0 +1,13 @@
+<?php
+
+
+function sum( $num1, $num2){
+    return "sum of numbers are:".$num1+$num2;
+}
+function sub( $num1, $num2){
+    return "subration of numbers are:".$num1-$num2;
+}
+
+function division($num1, $num2 ){
+    return "division of numbers are :".$num1/$num2;
+}
