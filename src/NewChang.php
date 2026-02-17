5441a4972f20cdd3ef711963f318d07b2d3c8d20
[Arif <arif.hussoun898@gmail.com>
2026-02-17 14:49:46 +0500

    next funciton added

--- src/NewChang.php
+++ src/NewChang.php
@@ -11,3 +11,16 @@ function sub( $num1, $num2){
 function division($num1, $num2 ){
     return "division of numbers are :".$num1/$num2;
 }
+
+function multiplication( $num1, $num2 ){
+    return "multiplications of numbers are :".$num1 *$num2."\n";
+}
+
+
+function getName(){
+    return "hey my name is ";
+}
+
+function getDefaultName( ){
+    return "Name";
+}
